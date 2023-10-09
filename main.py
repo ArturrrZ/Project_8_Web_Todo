ea5934e451136bf1aff29bf9661247d945167b9f
Artur Z <arturziianbaev@gmail.com>
2023-10-08 18:02:48 -0700

    ready to deploy

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, redirect, request
 from flask_bootstrap import Bootstrap5
 
+import os
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "afafafafas2131"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 
 
@@ -43,4 +45,4 @@ def delete(index):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
